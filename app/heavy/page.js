8297b1bb57ab848ae2276a1416c06f8b5bc46a36
Farjana0313 <farjanaakter178@gmail.com>
2024-05-30 16:25:19 +0600

    error.js'

--- app/heavy/page.js
+++ app/heavy/page.js
@@ -3,11 +3,13 @@ import Posts from "../components/Posts";
 
 export default function Heavy() {
     return (
-        <div className="p-4">
+        <div>
             <h1>Heavy page</h1>
-            <Suspense fallback={<h1>Loading Post....</h1>}>
-                <Posts />
+            <div>
+                <Suspense fallback={<h1>Loading posts...</h1>}>
+                    <Posts />
                 </Suspense>
+            </div>
         </div>
-    )
+    );
 }
--- app/layout.js
+++ app/layout.js
@@ -4,29 +4,35 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <nav className="border-b border-gray-400 p-4">
-          <ul className="flex gap-5">
-            <li>
-              <CustomLink path='/'>Home</CustomLink>
-            </li>
-            <li>
-              <CustomLink path='/dashboard/analytics'>Analytics</CustomLink>
-            </li>
-            <li>
-              <CustomLink path='/dashboard/settings' prefetch={false} >Settings</CustomLink>
-            </li>
-            <li>
-              <CustomLink path='/heavy' >Heavy</CustomLink>
-            </li>
-          </ul>
-        </nav>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <nav className="p-4 border-b border-gray-400">
+                    <ul className="flex gap-5">
+                        <li>
+                            <CustomLink path="/">Home</CustomLink>
+                        </li>
+                        <li>
+                            <CustomLink path="/analytics">
+                                Analytics
+                            </CustomLink>
+                        </li>
+                        <li>
+                            <CustomLink
+                                prefetch={false}
+                                path="/settings"
+                            >
+                                Settings
+                            </CustomLink>
+                        </li>
+                        <li>
+                            <CustomLink path="/heavy">Heavy</CustomLink>
+                        </li>
+                    </ul>
+                </nav>
+                {children}
+            </body>
+        </html>
+    );
 }
