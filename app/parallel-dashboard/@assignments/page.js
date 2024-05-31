da512ff6b29b0b59f8cc5b357cc0877b65006761
Farjana0313 <farjanaakter178@gmail.com>
2024-05-31 18:33:15 +0600

    sdf

--- app/parallel-dashboard/@assignments/page.js
+++ app/parallel-dashboard/@assignments/page.js
@@ -1,7 +1,6 @@
-// import wait from "@/lib/wait";
-
+import wait from "@/lib/mian";
 export default async function AssignmentsPage() {
-    // await wait(3000);
+    await wait(3000);
     return (
         <div className="text-xl p-4 border border-gray-200 rounded h-[360px] flex items-center justify-center">
             ASSIGNMENTS
