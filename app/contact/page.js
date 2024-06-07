3ede0e0cd2da9f67a9cab373dafeadde77bd3390
Farjana Akter <farjanaakter178@gmail.com>
2024-06-07 08:04:47 +0600

    intercepting route done

---
+++ app/contact/page.js
@@ -0,0 +1,8 @@
+
+export default function ContactPage() {
+    return (
+        <div className="p-20">
+            <h1 className="text-2xl">ContactPage</h1>
+        </div>
+    )
+}
--- app/folder1/folder4/(..)folder3/page.js
+++ app/folder1/folder4/(..)folder3/page.js
@@ -2,7 +2,7 @@
 export default function Folder3() {
     return (
         <div className="p-20">
-            <h1 className="text-2xl">Folder 3</h1>
+            <h1 className="text-2xl">Interceptin Folder 3</h1>
         </div>
     )
 }
