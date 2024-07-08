f8ca01ba39e03a2a2e15cc4963d3c0db4ac2a124
Farjana0313 <farjanaakter178@gmail.com>
2024-07-08 18:16:41 +0600

    fetch api

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import getJoke from "@/utils/getJoke";
+import { cookies } from "next/headers";
+import Image from "next/image";
+
+export default async function Home() {
+  const theme = cookies().get("theme");
+  const joke = await getJoke();
+  console.log('joke', joke);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+     {joke.value}
+    </main>
+  );
+}
---
+++ utils/getJoke.js
@@ -0,0 +1,8 @@
+export default async function getJoke(){
+    const res = await fetch('https://api.chucknorris.io/jokes/random');
+    if (!res.ok) {
+        throw new Error("Fetch error ...."); 
+    }
+
+    return res.json();
+}
